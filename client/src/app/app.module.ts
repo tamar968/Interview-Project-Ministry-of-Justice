75528f4b36446e7212ad989057f40b6349a6f10c
tamar968 <52313308+tamar968@users.noreply.github.com>
2022-05-05 23:36:32 +0300

    update modules imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContributionModule } from './modules/contribution/contribution.module';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ContributionModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/modules/contribution/contribution.module.ts
+++ client/src/app/modules/contribution/contribution.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
 import { AddContributionComponent } from 'src/app/components/add-contribution/add-contribution.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -10,8 +9,8 @@ import { DonationsListComponent } from 'src/app/components/donations-list/donati
 import { DonationCardComponent } from 'src/app/components/donation-card/donation-card.component';
 import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select'
-import { HttpClientModule } from '@angular/common/http';
 import { ContributionRoutingModule } from '../../routes/contribution-routing.module';
+
 @NgModule({
   declarations: [
     AddContributionComponent,
@@ -22,13 +21,11 @@ import { ContributionRoutingModule } from '../../routes/contribution-routing.mod
     CommonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule,
     MatIconModule,
     MatSelectModule,
     MatOptionModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
     ContributionRoutingModule
   ]
 })
